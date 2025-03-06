d54a8a8de3a96b91d2d82b5719b70002cddf1f8d
Suraj-raut <rautsuraj14@gmail.com>
2025-03-06 19:45:19 +0530

    Mediation Ads Initialized

--- Assets/Scripts/GameScripts/WordGuessGame.cs
+++ Assets/Scripts/GameScripts/WordGuessGame.cs
@@ -31,7 +31,7 @@ public class WordGuessGame : MonoBehaviour
         GetTargetWord();
          //submitButton.onClick.AddListener(CheckGuess);
 
-         AdsManager.Instance.bannerAds.ShowBannerAd();
+        // AdsManager.Instance.bannerAds.ShowBannerAd();
     }
     
 
@@ -58,12 +58,12 @@ public class WordGuessGame : MonoBehaviour
     private void OnEnable()
     {
         KeyboardInputManager.OnWordComplete += CheckGuessIsValid;
-        AdsManager.Instance.rewardedAds.OnRewardedAdCompleted += RewardThePlayer;
+       // AdsManager.Instance.rewardedAds.OnRewardedAdCompleted += RewardThePlayer;
     }
     private void OnDisable()
     {
          KeyboardInputManager.OnWordComplete -= CheckGuessIsValid;
-         AdsManager.Instance.rewardedAds.OnRewardedAdCompleted -= RewardThePlayer;
+       //  AdsManager.Instance.rewardedAds.OnRewardedAdCompleted -= RewardThePlayer;
     }
 
     private void CheckGuessIsValid(string guess)
@@ -135,7 +135,7 @@ public class WordGuessGame : MonoBehaviour
             _scoreManger.SaveTheHighScore();
             resultPanel.gameObject.SetActive(true);
             resultPanel.PlayerWon();
-            AdsManager.Instance.bannerAds.HideBannerAd();
+            //AdsManager.Instance.bannerAds.HideBannerAd();
             // Handle win condition
         }
         else if (attempts == 6)
@@ -146,7 +146,7 @@ public class WordGuessGame : MonoBehaviour
             _scoreManger.SaveTheHighScore();
             resultPanel.gameObject.SetActive(true);
             resultPanel.PlayerLose(targetWord);
-            AdsManager.Instance.bannerAds.HideBannerAd();
+           // AdsManager.Instance.bannerAds.HideBannerAd();
             // Handle game over
         }
     }
@@ -162,11 +162,11 @@ public class WordGuessGame : MonoBehaviour
         ResetKeyboardKeysColor();
         submitButton.InCompleteWord();
         GetTargetWord();
-        AdsManager.Instance.bannerAds.ShowBannerAd();
+        //AdsManager.Instance.bannerAds.ShowBannerAd();
 
         if(gamePlayedCount % 3 == 0)
         {
-            AdsManager.Instance.interstitialAds.ShowInterstitialAd();
+            //AdsManager.Instance.interstitialAds.ShowInterstitialAd();
         }
     }
 
@@ -196,7 +196,7 @@ public class WordGuessGame : MonoBehaviour
     public void OnClickHintButton()
     {
         AudioManager.Instance.PlayPopupOpenSound();
-        AdsManager.Instance.rewardedAds.ShowRewardedAd();
+        //AdsManager.Instance.rewardedAds.ShowRewardedAd();
         
     }
 
@@ -204,31 +204,31 @@ public class WordGuessGame : MonoBehaviour
     {
         bool isRewardGiven = false;
 
-        if(AdsManager.Instance.rewardedAds.isRewarded)
-        {
-            if(!isRewardGiven)
-            {
-                foreach (KeyboardButtonController key in allKeys)
-                {
-                    for (int i = 0; i < targetWord.Length; i++)
-                    {
-                        if (targetWord.Contains(key.keyLetter) && !isRewardGiven)
-                        {
-                            Color color = key.GetTheKeyColor();
+        // if(AdsManager.Instance.rewardedAds.isRewarded)
+        // {
+        //     if(!isRewardGiven)
+        //     {
+        //         foreach (KeyboardButtonController key in allKeys)
+        //         {
+        //             for (int i = 0; i < targetWord.Length; i++)
+        //             {
+        //                 if (targetWord.Contains(key.keyLetter) && !isRewardGiven)
+        //                 {
+        //                     Color color = key.GetTheKeyColor();
                             
-                            if(color != Color.green && color != Color.yellow)
-                            {
-                                key.SetContainerFillColor(Color.yellow);
-                                isRewardGiven = true;
-                                break;
-                            }
+        //                     if(color != Color.green && color != Color.yellow)
+        //                     {
+        //                         key.SetContainerFillColor(Color.yellow);
+        //                         isRewardGiven = true;
+        //                         break;
+        //                     }
                         
-                        }
-                    }
-                }
-            }
+        //                 }
+        //             }
+        //         }
+        //     }
 
-        }
+        // }
 
     }
 
