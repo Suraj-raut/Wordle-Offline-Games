876583aadb437b264e0748e92af54390dc8d74d1
Suraj-raut <rautsuraj14@gmail.com>
2025-03-10 20:49:42 +0530

    Firebase database Implemented

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -1,6 +1,6 @@
-// using Firebase.Database;
-// using Firebase.Extensions;
-// using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using Firebase;
 using UnityEngine;
 using System.Collections.Generic;
 using System;
@@ -13,7 +13,7 @@ public class DatabaseManager : MonoBehaviour
     private string wordCountPath = "TotalWordsCount"; //Path to the word count
     private int numWordsToFetch = 100;
     private long totalWordCount = 3646;
-  //  private DatabaseReference reference;
+    private DatabaseReference reference;
 
     public List<string> OnlineWordsList = new List<string>();
 
@@ -40,7 +40,7 @@ public class DatabaseManager : MonoBehaviour
 
     void Start()
     {
-       // reference = FirebaseDatabase.DefaultInstance.RootReference;; // Get root reference
+        reference = FirebaseDatabase.DefaultInstance.RootReference;; // Get root reference
         Debug.Log("Total number of words-->Start : " + totalWordCount);
 
     }
@@ -67,30 +67,30 @@ public class DatabaseManager : MonoBehaviour
     {
         AudioManager.Instance.PlayPopupOpenSound();
 
-        // if(databaseNode != null) { databasePath = databaseNode; }  
+        if(databaseNode != null) { databasePath = databaseNode; }  
 
-        //  Debug.Log("GetTotalWordsCount: of language " + databaseNode);
+         Debug.Log("GetTotalWordsCount: of language " + databaseNode);
 
-        // //Fetch the total number of words
-        // reference.Child(databaseNode).Child(wordCountPath).GetValueAsync().ContinueWith(task => {
-        //     if (task.IsFaulted || task.IsCanceled)
-        //     {
-        //         Debug.Log("Total number of words--->From database : " + totalWordCount);
-        //         HandleError(task);
-        //         return;
-        //     }
+        //Fetch the total number of words
+        reference.Child(databaseNode).Child(wordCountPath).GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.Log("Total number of words--->From database : " + totalWordCount);
+                HandleError(task);
+                return;
+            }
 
-        //      totalWordCount = (long)task.Result.Value;
-        //       Debug.Log("Total number of words-->After fetching : " + totalWordCount);
+             totalWordCount = (long)task.Result.Value;
+              Debug.Log("Total number of words-->After fetching : " + totalWordCount);
 
-        //      //Generate a list of random unique indices to fetch words
-        //      List<long> randomIndices = GenerateRandomUniqueIndices(totalWordCount, numWordsToFetch);
+             //Generate a list of random unique indices to fetch words
+             List<long> randomIndices = GenerateRandomUniqueIndices(totalWordCount, numWordsToFetch);
 
-        //     //Now fetch the words based on random indices
-        //      FetchWordsByIndices(randomIndices);
+            //Now fetch the words based on random indices
+             FetchWordsByIndices(randomIndices);
            
 
-        // });
+        });
            
            
     }
@@ -118,44 +118,64 @@ public class DatabaseManager : MonoBehaviour
     {
         
         counter = 0;
-        // foreach (long index in indices)
-        // {
-        //     reference.Child(databasePath).Child(index.ToString()).GetValueAsync().ContinueWithOnMainThread(task => {
-        //         if (task.IsFaulted || task.IsCanceled)
-        //         {
-        //             HandleError(task);
-        //             return;
-        //         }
-        //         else if (task.IsCompleted)
-        //        {
+        foreach (long index in indices)
+        {
+            reference.Child(databasePath).Child(index.ToString()).GetValueAsync().ContinueWithOnMainThread(task => {
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    HandleError(task);
+                    return;
+                }
+                else if (task.IsCompleted)
+               {
 
-        //             string word = (string)task.Result.Value;
-        //             OnlineWordsList.Add(word);
-        //             counter++;
-        //             Debug.Log("Word added : " + word);
+                    string word = (string)task.Result.Value;
+                    OnlineWordsList.Add(word);
+                    counter++;
+                    Debug.Log("Word added : " + word);
 
-        //        }
+               }
 
  
-        //         Debug.Log("Counter of words -->>" + counter);
-        //         if(counter == indices.Count)
-        //         {
+                Debug.Log("Counter of words -->>" + counter);
+                if(counter == indices.Count)
+                {
 
-        //             EnterOnlineGameMode();
+                    EnterOnlineGameMode();
 
-        //         }
+                }
              
-        //     });
+            });
 
 
+        }
+    }     
+
+    void HandleError(Task task)
+    {
+        Debug.LogError("Error fetching data: " + task.Exception);
     }
 
-        
+    public void QueryWord(string wordToCheck)
+    {
+         reference.Child(databasePath).OrderByValue().EqualTo(wordToCheck).GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
 
-    // void HandleError(Task task)
-    // {
-    //     Debug.LogError("Error fetching data: " + task.Exception);
-    // }
+                if (snapshot.Exists)
+                {
+                    Debug.Log("Word found: " + wordToCheck);
+                }
+                else
+                {
+                    Debug.Log("Word NOT found.");
+                }
+            }
+        });
+    }
+
 
 }
 
